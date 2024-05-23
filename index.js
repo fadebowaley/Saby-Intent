fc038e05b40cd573b1c4bd95fc2f89528ad25d34
Ademola Adebowale <fadebowaley@gmail.com>
2024-05-23 09:58:42 +0100

    adding of files to intents

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { IntentRecognizer } = require("./intentRecognizer");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
